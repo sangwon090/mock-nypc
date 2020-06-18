e039641068e68e43260a3e05ba2b41be7d57a32b
Sangwon Ryu <bdboy090@naver.com>
2020-06-18 20:50:44 +0900

    Create boilerplate

---
+++ app.js
@@ -0,0 +1,13 @@
+const config = require('./config');
+const express = require('express');
+
+const app = express();
+const router = require('./router/main')(app);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+const server = app.listen(config.server.port, () => {
+    console.log(`[i] mock-nypc 서버가 포트 ${config.server.port}에서 시작되었습니다.`);
+});
