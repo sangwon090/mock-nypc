595135ba640185d7da5527209ffc7562e6061f6f
Sangwon Ryu <bdboy090@naver.com>
2020-06-18 21:11:02 +0900

    Apply web fonts

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const router = require('./router/main')(app);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+app.use('/static', express.static('static'));
 
 const server = app.listen(config.server.port, () => {
     console.log(`[i] mock-nypc 서버가 포트 ${config.server.port}에서 시작되었습니다.`);
