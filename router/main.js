e6a142d12386c218b68c523c0cb0da4b09f7c7fc
Sangwon Ryu <bdboy090@naver.com>
2020-06-19 23:02:05 +0900

    공지사항 페이지 생성

--- router/main.js
+++ router/main.js
@@ -6,6 +6,14 @@ module.exports = (app, config) => {
         });
     });
 
+    app.get('/notice', (req, res) => {
+        res.render('notice', {
+            title: config.title,
+            logined: false,
+            data: []
+        });
+    });
+
     app.get('/login', (req, res) => {
         // TODO: 이미 로그인 되었으면 홈으로 리다이렉트
         res.render('login', {
@@ -13,4 +21,12 @@ module.exports = (app, config) => {
             logined: false
         });
     });
+
+    app.get('/register', (req, res) => {
+        // TODO: 이미 로그인 되었으면 홈으로 리다이렉트
+        res.render('register', {
+            title: config.title,
+            logined: false
+        });
+    })
 };
