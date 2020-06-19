b636d8d5162f1253595494c1c9a361ed9692d84b
Sangwon Ryu <bdboy090@naver.com>
2020-06-19 22:15:56 +0900

    로그인 페이지 생성

--- router/main.js
+++ router/main.js
@@ -2,7 +2,15 @@ module.exports = (app, config) => {
     app.get('/', (req, res) => {
         res.render('home', {
             title: config.title,
-            logined: true
+            logined: false
+        });
+    });
+
+    app.get('/login', (req, res) => {
+        // TODO: 이미 로그인 되었으면 홈으로 리다이렉트
+        res.render('login', {
+            title: config.title,
+            logined: false
         });
     });
 };
