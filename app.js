f638b86ebf4fe33651cfcee4ae248883323ff243
Sangwon Ryu <bdboy090@naver.com>
2020-06-19 16:48:46 +0900

    Create error page

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const config = require('./config');
 const express = require('express');
 
 const app = express();
-const router = require('./router/main')(app);
+const router = require('./router/main')(app, config);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use('/static', express.static('static'));
+app.use((err, req, res, next) => {
+    if(err) {
+        res.render('error', {
+            showError: config.showError,
+            error: err
+        });
+    }
+})
 
 const server = app.listen(config.server.port, () => {
     console.log(`[i] mock-nypc 서버가 포트 ${config.server.port}에서 시작되었습니다.`);
