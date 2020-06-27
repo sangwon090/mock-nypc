4e9156e6b50624c5074ac8e6facf0874cf0776ba
Sangwon Ryu <bdboy090@naver.com>
2020-06-28 03:51:17 +0900

    회원가입 폼 검사

--- router/api.js
+++ router/api.js
@@ -1,3 +1,26 @@
 module.exports = (app, config) => {
+    app.post('/api/register', (req, res) => {
+        const username_regex = /^[\w\Wㄱ-ㅎㅏ-ㅣ가-힣]{5,20}$/g;
+        const email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+        console.log(req.body);
+
+        if(!req.body.username || !req.body.email || !req.body.password || !req.body.password2) {
+            // Abnormal access
+            res.redirect('/register/?error=1');
+        } else if(!username_regex.test(req.body.username)) {
+            res.redirect('/register/?error=2');
+        } else if(!email_regex.test(req.body.email)){
+            res.redirect('/register/?error=3');
+        } else if(req.body.password != req.body.password2) {
+            res.redirect('/register/?error=4');
+        } else if(req.body.password.length < 8) {
+            res.redirect('/register/?error=5');
+        } else if(req.body.agreed != 'true') {
+            // Abnormal access
+            res.redirect('/register/?error=6');
+        } else {
+            res.redirect('/');
+        }
+    });
 };
--- router/main.js
+++ router/main.js
@@ -28,12 +28,21 @@ module.exports = (app, config, rootPath) => {
         // TODO: 이미 로그인 되었으면 홈으로 리다이렉트
         let tos = fs.readFileSync(rootPath + '/docs/terms_of_service.txt').toString();
         let pri = fs.readFileSync(rootPath + '/docs/privacy_policy.txt').toString();
+        let error = '';
+
+        if(req.query.error == 1) error = '입력하지 않은 항목이 있습니다.';
+        else if(req.query.error == 2) error = '닉네임이 올바르지 않습니다.\\n(한글/영문/기호 5~20자)';
+        else if(req.query.error == 3) error = '이메일이 올바르지 않습니다.';
+        else if(req.query.error == 4) error = '패스워드가 일치하지 않습니다.';
+        else if(req.query.error == 5) error = '패스워드가 너무 짧습니다.';
+        else if(req.query.error == 6) error = '이용약관을 동의하지 않으셨습니다.';
 
         res.render('register', {
             title: config.title,
             logined: false,
             tos: tos,
-            pri: pri
+            pri: pri,
+            error: error
         });
     })
 };
